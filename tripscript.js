0b71b46ca6029a0217f54ab35166772dd7582729
DHANESVARAN M <143826819+DHANESVARAN@users.noreply.github.com>
2024-11-12 15:08:56 +0530

    Add files via upload

---
+++ tripscript.js
@@ -0,0 +1,6 @@
+const totalFoodTrip = parseFloat(localStorage.getItem("totalFoodTrip")) || 0;
+const totaltravelTrip = parseFloat(localStorage.getItem("totaltravelTrip")) || 0;
+const totalacoomodationTrip = parseFloat(localStorage.getItem("totalaccomodationTrip")) || 0;
+const totalOtherTrip =  parseFloat(localStorage.getItem("totalotherTrip")) || 0;
+const totalEntertainmentTrip = parseFloat(localStorage.getItem("totalentertainmentTrip")) || 0;
+
